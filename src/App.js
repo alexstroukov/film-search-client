9c018de7667fb9bc4265b48ae37630edd199f049
Slex <alexstroukov@yahoo.co.uk>
2019-03-20 20:12:05 +0000

    added theming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 import store from './store'
 import Header from './containers/Header'
 import FilmsPage from './pages/FilmsPage'
@@ -9,13 +12,16 @@ import PageNotFound from './pages/PageNotFound'
 export default () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={FilmsPage} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </div>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={FilmsPage} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
     </BrowserRouter>
   </Provider>
 )
